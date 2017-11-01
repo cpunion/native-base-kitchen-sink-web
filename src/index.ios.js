dde970300f93e221bde6fee562d102d59d961166
Li Jie <lijie@applean.cn>
2017-11-01 12:16:45 +0800

    Render with AppRegistry

--- src/index.ios.js
+++ src/index.ios.js
@@ -2,5 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppRegistry } from 'react-native';
 import App from './App';
-render(<App />, document.getElementById('root'))
+// render(<App />, document.getElementById('root'))
 AppRegistry.registerComponent('NativebaseKitchenSink', () => App);
+AppRegistry.runApplication('NativebaseKitchenSink', {
+  rootTag: document.getElementById('root')
+});
